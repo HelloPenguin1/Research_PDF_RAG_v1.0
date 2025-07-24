6ffd81abcce65f80696549f951c6a09cfdd55ad3
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-07-24 23:24:07 +0530

    debug

--- document_processor.py
+++ document_processor.py
@@ -1,9 +1,10 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from config import Config, set_environment
+
+set_environment()
 
 class Document_Processor:
     def __init__(self, embeddings = None):
