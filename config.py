dc8b0d45a0c38c564a7777a3eea6ef8ffc5c3fc6
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-07-24 23:10:34 +0530

    Add safe config.py for deployment

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from langchain_huggingface import HuggingFaceEmbeddings
+import streamlit as st
+
+def set_environment():
+    secret_keys = [
+        "OPENAI_API_KEY",
+        "LANGCHAIN_API_KEY",
+        "LANGCHAIN_PROJECT",
+        "HF_TOKEN",
+        "GROQ_API_KEY"
+    ]
+    for key in secret_keys:
+        if key in st.secrets:
+            os.environ[key] = st.secrets[key]
+
+class Config:
+    @staticmethod
+    def getembeddings():
+        return HuggingFaceEmbeddings(model="all-MiniLM-L6-v2")
+    
+    @staticmethod   
+    def get_temp_pdf_path():
+       return "./temp.pdf"
+    
+       
+    
+   
+   
+      
